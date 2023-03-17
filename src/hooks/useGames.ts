88cd18e4a4b1b7b14d90a5d5b74f1337b10dd0b9
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-03-17 20:04:09 +0500

    👌 IMPROVE: added skeleton loaders on game cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -23,16 +23,21 @@ interface FetchGamesResponse {
 function useGames() {
   const [games, setGames] = useState<Games[]>([]);
   const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     apiClient
       .get<FetchGamesResponse>('/games')
-      .then(res => setGames(res.data.results))
+      .then(res => {
+        setGames(res.data.results);
+        setIsLoading(false);
+      })
       .catch(error => {
         setError(error.message);
+        setIsLoading(false);
       });
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 }
 export default useGames;
