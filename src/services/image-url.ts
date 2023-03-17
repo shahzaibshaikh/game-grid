cf29119bd43a50f47eedb4b0e6d0ac6ad8f52a10
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-03-17 19:50:28 +0500

    👌 IMPROVE: getting optimized images

---
+++ src/services/image-url.ts
@@ -0,0 +1,9 @@
+function getCroppedImageUrl(url: string) {
+  const target = 'media/';
+  const index = url.indexOf(target) + target.length;
+  const newUrl = url.slice(0, index) + 'crop/600/400/' + url.slice(index);
+
+  return newUrl;
+}
+
+export default getCroppedImageUrl;
