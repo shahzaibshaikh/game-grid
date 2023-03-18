5147d17189d9b9344fe02e6509f4d3fffb08540f
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-03-18 19:53:48 +0500

    🐛 FIX: handled games with no images

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import placeholderImage from '../assets/no-image-placeholder-6f3882e0.webp';
+
 function getCroppedImageUrl(url: string) {
-  if (!url) return '';
+  if (!url) return placeholderImage;
   const target = 'media/';
   const index = url.indexOf(target) + target.length;
   const newUrl = url.slice(0, index) + 'crop/600/400/' + url.slice(index);
