7cebbfb7075fabf5161fe5aeb178e24913ce0cb2
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-03-18 22:31:53 +0500

    📦 NEW: added search input

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image, Text } from '@chakra-ui/react';
 import Logo from '../assets/logo.webp';
 import ColorModeSwitch from './ColorModeSwitch';
+import SearchInput from './SearchInput';
 
 function NavBar() {
   return (
     <HStack justifyContent='space-between'>
       <Image src={Logo} boxSize='60px' marginLeft={-2} />
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
