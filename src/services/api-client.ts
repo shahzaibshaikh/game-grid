0bab1977c05e7c462fce94686c4b38c74a7a755c
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-03-23 03:34:13 +0500

    ✋ moved api key to .env

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: '07eef8e2c55a4106a10db5ac5f5065b6'
+    key: import.meta.env.VITE_API_KEY
   }
 });
